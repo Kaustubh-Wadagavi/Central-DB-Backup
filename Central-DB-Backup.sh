9cbc65c1d271ddcca796c95f93d35dcca49334de
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-06-25 16:35:57 +0530

    Final Changes are Done.

--- Central-DB-Backup.sh
+++ Central-DB-Backup.sh
@@ -2,6 +2,7 @@
 
 DB_USER=root
 DB_PASS=Krish!@3agni
+BACKUP_FOLDER=/home/krishagni/Desktop/Backups/
 
 # Declared Database names 
 declare -a DB_NAMES=(
@@ -16,10 +17,10 @@ takeBackup()
    do 
      echo ""
      echo "Tacking ${DB_NAMES[$index]} Backup"
-     mysqldump -u$DB_USER -p$DB_PASS --single-transaction --skip-lock-tables --routines "${DB_NAMES[$index]}" | gzip > /home/krishagni/Desktop/Backups/${DB_NAMES[$index]}_`date +\%d-\%m-\%Y`.SQL.gz
+     mysqldump -u$DB_USER -p$DB_PASS --single-transaction --skip-lock-tables --routines "${DB_NAMES[$index]}" | gzip > $BACKUP_FOLDER/${DB_NAMES[$index]}_`date +\%d-\%m-\%Y`.SQL.gz
    	
      echo  "Deleting 30 days older files from Backups"
-     find /home/krishagni/Desktop/Backups/ -mtime +30 -exec rm {} \;
+     find $BACKUP_FOLDER -mtime +30 -exec rm {} \;
 
    done
 }
